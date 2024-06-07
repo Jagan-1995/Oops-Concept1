d76f0d10d138482b6d91f65f17b6d1a33304386c
Jagan Baskar <jackjagan1995@gmail.com>
2024-06-07 08:40:01 +0530

    Added the code for Producer and Consumer with Synchronization

---
+++ src/main/java/ProducerConsumer/Store.java
@@ -0,0 +1,39 @@
+package ProducerConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class Store {
+
+    private int maxSize;
+
+    private List<Object> items;
+
+    Store(int maxSize){
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+    public int getMaxSize(){
+        return maxSize;
+    }
+
+    public List<Object> getItems(){
+        return items;
+    }
+
+    public void addItem(){
+        System.out.println("Producer production time, current size" + this.items.size());
+        this.items.add(new Object());
+    }
+
+    public synchronized void removeItem(){
+        // 3 threads are running the remove() method at the same time
+        // [2,3] -> []
+        System.out.println("Consumer consuming time, current size " + this.items.size());
+        this.items.remove(this.items.size() - 1);
+    }
+
+}

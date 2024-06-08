e1a2671e7505d27a729f5495df27dda757a3b76c
Jagan Baskar <jackjagan1995@gmail.com>
2024-06-09 00:19:27 +0530

    Added code for the Producer Consumer Semaphores

---
+++ src/main/java/ProducerConsumerSemaphores/Client.java
@@ -0,0 +1,25 @@
+package ProducerConsumerSemaphores;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ExecutorService ex = Executors.newCachedThreadPool();
+        Store store =  new Store(5);
+
+        Semaphore prodSemaphore = new Semaphore(5);
+        Semaphore consSemaphore = new Semaphore(0);
+
+        // 8 producer
+        for (int i = 1; i <= 8; i++){
+            ex.execute(new Producer(store, prodSemaphore, consSemaphore));
+        }
+        // 20 consumer
+        for (int i = 1; i <= 20; i++){
+            ex.execute(new Consumer(store, prodSemaphore, consSemaphore));
+        }
+    }
+}
